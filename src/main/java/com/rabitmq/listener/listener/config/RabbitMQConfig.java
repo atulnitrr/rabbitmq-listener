f7dedf4d80d3834b30c7fec723736e5ba890190a
atulnitrr <atulkumar458@gmail.com>
2019-08-08 01:55:13 +0530

    Create binding

--- src/main/java/com/rabitmq/listener/listener/config/RabbitMQConfig.java
+++ src/main/java/com/rabitmq/listener/listener/config/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.rabitmq.listener.listener.config;
 
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -21,6 +25,27 @@ public class RabbitMQConfig {
     Queue myQueue() {
         return new Queue(MY_QUEUE, true);
     }
+
+    @Bean
+    Exchange exchangeTopoic() {
+        return ExchangeBuilder.topicExchange("MyTopicExchange")
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    Binding binding() {
+//        return new Binding(MY_QUEUE, Binding.DestinationType.QUEUE, "MyTopicExchange", "topic", null);
+        return BindingBuilder.bind(myQueue())
+                .to(exchangeTopoic())
+                .with("topic")
+                .noargs();
+    }
+
+
+
+
+
 
     @Bean
     ConnectionFactory connectionFactory() {
